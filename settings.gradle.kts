fe5dee0d55a0753c5a50bbb919115835bf50fc65
Gjum <code.gjum@gmail.com>
2026-02-23 21:35:27 +0100

    boilerplate

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
+        maven("https://maven.parchmentmc.org") { name = "ParchmentMC" }
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.7-beta.7"
+}
+
+stonecutter {
+    create(rootProject) {
+        versions(
+            "1.21.11",
+        )
+        vcsVersion = "1.21.11"
+    }
+}
+
+rootProject.name = "CivCommon"
---
+++ stonecutter.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("dev.kikugie.stonecutter")
+    id("fabric-loom") version "1.15-SNAPSHOT" apply false
+    // id("me.modmuss50.mod-publish-plugin") version "0.8.+" apply false // Publishes builds to hosting websites
+}
+
+stonecutter active "1.21.11"
+
+// See https://stonecutter.kikugie.dev/wiki/config/params
+stonecutter parameters {
+    swaps["mod_version"] = "\"" + property("mod.version") + "\";"
+    swaps["minecraft"] = "\"" + node.metadata.version + "\";"
+    // constants["release"] = property("mod.id") != "template" // TODO git status
+    dependencies["fapi"] = node.project.property("deps.fabric_api") as String
+}
+
+/*
+// Make newer versions be published last
+stonecutter tasks {
+    order("publishModrinth")
+    order("publishCurseforge")
+}
+ */
